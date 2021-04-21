c8e5444d1804b1db49da8797681ba11f0386fb79
md umar faruk <mdumar11233>
2021-04-21 21:35:27 +0600

    react reduce hook redux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Reducer from './Components/Reducer/Reducer';
+
+function App() {
+  return (
+    <div className="App">
+      <Reducer></Reducer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Reducer/Reducer.js
@@ -0,0 +1,25 @@
+import React, { useReducer } from 'react';
+
+const initialSate = {count: 0}
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            return {count: state.count + 1}
+        break;   
+        default:
+            return state;     
+    }
+}
+
+const Reducer = () => {
+    const [state, dispatch] = useReducer(reducer, initialSate);
+    return (
+        <div>
+            <h2>This is reducer count: {state.count}</h2>
+            <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
+        </div>
+    );
+};
+
+export default Reducer;
