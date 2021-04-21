480ac870aa50bf8e43726210653c190710e94095
md umar faruk <mdumar11233>
2021-04-21 23:17:44 +0600

    added new code

--- src/Components/PatientReducer/PatientReducer.js
+++ src/Components/PatientReducer/PatientReducer.js
@@ -1,16 +1,31 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useRef } from 'react';
 import { patientReducer, patientSate } from '../PatientReducers/PatientReducers';
 
 
 
 const PatientReducer = () => {
+    const nameRef = useRef();
     const [state, dispatch] = useReducer(patientReducer, patientSate);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'ADD_PATIENT', 
+            name: nameRef.current.value, 
+            id: state.patients.length + 1
+        })
+        nameRef.current.value = '';
+    }
     return (
         <div>
             <h1>Manage Doctor Chamber: {state.patients.length}</h1>
-            <form action="">
-                <input type="text"/>
+            <form action="" onSubmit={handleSubmit}>
+                <input ref={nameRef}/>
+                {/* <input type="submit" value="Submit"/> */}
             </form>
+            {
+                state.patients.map(pt => <li onClick={() => dispatch({type: 'REMOVE_PATIENT', id: pt.id})}>{pt.name}</li>)
+            }
         </div>
     );
 };
--- src/Components/PatientReducers/PatientReducers.js
+++ src/Components/PatientReducers/PatientReducers.js
@@ -6,10 +6,18 @@ export const patientReducer = (state, action) => {
 
     switch(action.type){
         case 'ADD_PATIENT':
-            return state;
+            const newPatient = {
+                id: action.id,
+                name: action.name
+            }
+            const allPatients = [...state.patients, newPatient];
+            return {patients: allPatients};
             break;
         case 'REMOVE_PATIENT':
-            return state;
+            console.log(action);
+            const remaining = state.patients.filter(pt => pt.id !== action.id);
+            const newState = {patients: remaining}
+            return newState;
         default:
             return state;
     }
